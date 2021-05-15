614c84530200aa34f30d7c0388d183f3598f4c20
Yasuhiro Yamaguchi <altescy@fastmail.com>
2021-05-15 16:15:54 +0900

    modify download method

--- minato/minato.py
+++ minato/minato.py
@@ -2,7 +2,7 @@ from contextlib import contextmanager
 from pathlib import Path
 from typing import IO, Any, Iterator, Optional, Union
 
-from minato.cache import Cache, CachedFile
+from minato.cache import Cache
 from minato.config import Config
 from minato.util import extract_path, is_local, open_file
 
@@ -50,28 +50,27 @@ class Minato:
             return filename
 
         url = str(url_or_filename)
-        cached_file = self.download(url)
-        local_path = cached_file.local_path
-
-        return local_path
-
-    def download(self, url: str, update: bool = False) -> CachedFile:
         if url in self._cache:
             cached_file = self._cache.by_url(url)
+            if not update:
+                return cached_file.local_path
         else:
             with self._cache.tx() as tx:
                 cached_file = tx.add(url)
 
-        with open(cached_file.local_path, "wb") as local_file:
-            with open_file(url, "rb") as remote_file:
-                content = remote_file.read()
-                local_file.write(content)
+        self.download(cached_file.url, cached_file.local_path)
 
         if update:
             with self._cache.tx() as tx:
                 tx.update(cached_file)
 
-        return cached_file
+        return cached_file.local_path
+
+    def download(self, url: str, filename: Path) -> None:
+        with open(filename, "wb") as local_file:
+            with open_file(url, "rb") as remote_file:
+                content = remote_file.read()
+                local_file.write(content)
 
     def remove(self, id_or_url: Union[int, str]) -> None:
         if isinstance(id_or_url, int):
