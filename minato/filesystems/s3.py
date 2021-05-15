a48b060b9615c2b0d8df0257968e35031fae4e28
Yasuhiro Yamaguchi <altescy@fastmail.com>
2021-05-16 01:55:38 +0900

    use credentials

--- minato/filesystems/s3.py
+++ minato/filesystems/s3.py
@@ -1,11 +1,13 @@
+import os
 from contextlib import contextmanager
 from pathlib import Path
 from typing import IO, Any, Iterator, Union
-from urllib.parse import urlparse
 
+from fs.opener.parse import parse_fs_url
 from fs_s3fs import S3FS
 
 from minato.filesystems.filesystem import FileSystem
+from minato.util import get_parent_path_and_filename
 
 
 @FileSystem.register(["s3"])
@@ -18,18 +20,25 @@ class S3FileSystem(FileSystem):
     ) -> Iterator[IO[Any]]:
         url = str(filename)
 
-        parsed_url = urlparse(url)
-        path = parsed_url.path  # assume '<bucket>/<path>/<to>/<filename>'
+        parsed_url = parse_fs_url(url)
+        bucket_name, _, path = parsed_url.resource.partition("/")
+        dir_path, s3_filename = get_parent_path_and_filename(path)
 
-        splitted_path = path.split("/")
+        aws_access_key_id = parsed_url.username or os.environ.get("AWS_ACCESS_KEY_ID")
+        aws_secret_access_key = parsed_url.password or os.environ.get(
+            "AWS_SECRET_ACCESS_KEY"
+        )
+        endpoint_url = parsed_url.params.get(
+            "endpoint_url",
+            os.environ.get("MINATO_S3_ENDPOINT_URL"),
+        )
 
-        if len(splitted_path) < 2:
-            raise ValueError(f"Invalid URL: {url}")
-
-        bucket_name = splitted_path.pop(0)
-        s3_filename = splitted_path.pop(-1)
-        dir_path = "/".join(splitted_path) if splitted_path else "/"
-
-        with S3FS(bucket_name, dir_path=dir_path) as s3fs:
-            with s3fs.open(s3_filename) as fp:
+        with S3FS(
+            bucket_name,
+            dir_path=dir_path or "/",
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            endpoint_url=endpoint_url,
+        ) as s3fs:
+            with s3fs.open(s3_filename, mode) as fp:
                 yield fp
