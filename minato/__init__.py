a04499afea80c8a15b53f47f54f46fa1ea4074a6
Yasuhiro Yamaguchi <altescy@fastmail.com>
2021-05-15 16:16:07 +0900

    add download method

--- minato/__init__.py
+++ minato/__init__.py
@@ -5,6 +5,7 @@ from typing import IO, Any, Iterator, Union
 from minato.minato import Minato
 
 __version__ = "0.1.0"
+__all__ = ["Minato", "cached_path", "download", "open"]
 
 
 @contextmanager
@@ -20,3 +21,8 @@ def cached_path(
     url_or_filename: Union[str, Path],
 ) -> Path:
     return Minato().cached_path(url_or_filename)
+
+
+def download(url: str, filename: Union[str, Path]) -> None:
+    filename = Path(filename)
+    Minato().download(url, filename)
